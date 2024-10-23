3007e12ec002e67f6f1b61e127f1d6d6d5b3fb8f
紫斗 <zidou@stu.zzu.edu.cn>
2024-10-23 18:35:48 +0800

    upload

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# ZhiLianEXue-CheatProgress
-python程序之速刷【智联E学】-大学生职业规划短视频观看任务
+# python程序之速刷【智联E学】-大学生职业规划短视频观看任务
+
+## 效果展示
+
+![](http://lsky.zidoukn.cn/i/2024/10/23/6718d043dfe48.png)![](http://lsky.zidoukn.cn/i/2024/10/23/6718d043dfe48.png)
+![](http://lsky.zidoukn.cn/i/2024/10/23/6718cd662d663.png)
+
+## 前言
+
+![](http://lsky.zidoukn.cn/i/2024/10/23/6718c6456ba08.png)
+
+大学生久受职业规划课程之苦，在此分析网站后开发出了全自动并行刷课程的程序
+
+## 源码仓库地址
+
+[Github-zidou-kiyn/ZhiLianEXue-CheatProgress](https://github.com/zidou-kiyn/ZhiLianEXue-CheatProgress)
+
+## 使用教程
+
+### 配置文件-cookies & params
+
+1. 在浏览器端进入[智联E学](https://course.zhaopin.com/)网站，进入课程学习界面，**F12**打开浏览器开发者工具，切换到**网络**，**F5**刷新网页，在分类中选择**Fetch/XHR**![](http://lsky.zidoukn.cn/i/2024/10/23/6718ca393cea7.png)
+2. 点击如上图所示的请求中 `https://course.zhaopin.com/api/studentclasstask/getcourseinfo?******`，在标头处将cookie的内容复制到**cookies.txt**中![](http://lsky.zidoukn.cn/i/2024/10/23/6718cb12e66d2.png)![](http://lsky.zidoukn.cn/i/2024/10/23/6718cb4c174ab.png)
+3. 将载荷中各个参数对应的值替换到**params.json**中，注意，这边不是直接全部复制过去，需要对位替换![](http://lsky.zidoukn.cn/i/2024/10/23/6718cb8476665.png)![](http://lsky.zidoukn.cn/i/2024/10/23/6718cbf26ded9.png)
+
+### 启动程序
+
+配置好参数后即可按照下面两种方式运行，全自动完成视频任务，需要的时长**约为最长的视频的时长**，程序重新运行不会重新开始刷进度，可延续，所以直接运行即可，省心
+
+#### python运行
+
+使用清华源安装第三方库 `requests`、`loguru`
+
+```cmd
+pip install requests -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+pip install loguru -i https://pypi.tuna.tsinghua.edu.cn/simple
+```
+
+安装好后即可直接运行
+
+#### windows一键运行
+
+直接启动**main.exe**程序即可
---
+++ main.py
@@ -0,0 +1,185 @@
+import requests
+import json
+import time
+from loguru import logger
+
+# 过滤课程数据，提取必要的信息
+def filter_class_data(data):
+    filtered_data = {
+        "classTaskId": data["data"]["classTaskId"],
+        "id": data["data"]["id"],
+        "chapterDTOS": []
+    }
+
+    for chapter in data["data"]["chapterDTOS"]:
+        filtered_chapter = {
+            "id": chapter["id"],
+            "chapterLessions": []
+        }
+        for lesson in chapter["chapterLessions"]:
+            filtered_lesson = {
+                "fileName": lesson["fileName"],
+                "id": lesson["id"],
+                "newestProgress": lesson["newestProgress"],
+                "rate": lesson["rate"],
+                "totalTime": lesson["totalTime"]
+            }
+            filtered_chapter["chapterLessions"].append(filtered_lesson)
+        filtered_data["chapterDTOS"].append(filtered_chapter)
+
+    return filtered_data
+
+# 移除已完成的课程
+def remove_finished_lessons(class_info, finished_id):
+    for chapter in class_info["chapterDTOS"]:
+        chapter["chapterLessions"] = [
+            lesson for lesson in chapter["chapterLessions"] if lesson["id"] not in finished_id
+        ]
+
+# 移除没有课程的章节
+def remove_empty_chapters(class_info):
+    class_info["chapterDTOS"] = [
+        chapter for chapter in class_info["chapterDTOS"] if chapter["chapterLessions"]
+    ]
+
+# 加载cookies文件并转换为字典
+def load_cookies():
+    with open("./cookies.txt","r",encoding="utf-8") as f:
+        cookies_str = f.read()
+    cookies_dict = {}
+    for cookie in cookies_str.split('; '):
+        key, value = cookie.split('=', 1)
+        cookies_dict[key] = value
+    return cookies_dict
+
+# 根据cookies生成请求头
+def load_headers(cookies):
+    headers = {
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'priority': 'u=1, i',
+        'referer': 'https://course.zhaopin.com/',
+        'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'token': cookies['token'],
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+    }
+    return headers
+
+# 加载请求参数
+def load_params():
+    with open("./params.json","r",encoding="utf-8") as f:
+        params = json.loads(f.read())
+    return params
+
+# 获取课程信息并过滤
+def get_class_info(cookies, headers, params):
+    response = requests.get(
+        'https://course.zhaopin.com/api/studentclasstask/getcourseinfo',
+        params=params,
+        cookies=cookies,
+        headers=headers,
+    )
+
+    return filter_class_data(response.json())
+
+# 模拟进度上报
+def cheat_progress(cookies, headers, params,
+                   classId, chapterId, lessionId,
+                   courseId, classTaskId, reportTime,
+                   progress, reportType):
+    json_data = {
+        'classId': classId,
+        'chapterId': chapterId,
+        'lessionId': lessionId,
+        'courseId': courseId,
+        'classTaskId': classTaskId,
+        'reportTime': reportTime,
+        'progress': progress,
+        'reportType': reportType,
+    }
+    response = requests.post(
+        'https://course.zhaopin.com/api/studyrecord/studentlearnrecord',
+        params=params,
+        cookies=cookies,
+        headers=headers,
+        json=json_data,
+    )
+    return response
+
+
+# 加载cookies、请求头和参数
+cookies = load_cookies()
+headers = load_headers(cookies)
+params = load_params()
+
+finished_id = []  # 已完成课程的ID列表
+count = 0  # 循环计数器
+
+# 配置日志输出到文件，并设置文件大小达到1MB后自动轮转
+logger.add("./run.log", rotation="1 MB")  # 文件达到1MB后自动轮转
+
+try:
+    while True:
+        class_info = get_class_info(cookies, headers, params)
+
+        remove_finished_lessons(class_info, finished_id)  # 移除已完成课程
+        remove_empty_chapters(class_info)  # 移除空章节
+
+        if len(class_info["chapterDTOS"]) <= 0:
+            logger.info("视频任务完成 退出程序")
+            break
+
+        for chapter in class_info['chapterDTOS']:
+            for lession in chapter['chapterLessions']:
+                logger.info("当前课程-{}".format(lession['fileName']))
+                if lession['rate'] >= 1:
+                    finished_id.append(lession['id'])
+                    logger.info("当前课程已完成 课程id添加到完成列表中")
+                    continue
+
+                classId = params['classId']
+                chapterId = chapter['id']
+                lessionId = lession['id']
+                courseId = class_info['id']
+                classTaskId = class_info['classTaskId']
+
+                if count <= 0:
+                    reportTime = int(time.time())
+                    progress = lession['newestProgress']
+                    reportType = 1
+                    response = cheat_progress(cookies, headers, params,
+                                   classId, chapterId, lessionId,
+                                   courseId, classTaskId, reportTime,
+                                   progress, reportType)
+                    logger.info("当前课程尚未开始观看 开始请求")
+                    if response.status_code != 200:
+                        logger.error("开始观看失败 报错信息:{}".format(response.text))
+                    continue
+
+                if lession['newestProgress'] > 0:
+                    reportTime = int(time.time())
+                    progress = lession['newestProgress'] + 30
+                    if progress > lession['totalTime']:
+                        progress = lession['totalTime']
+                    reportType = 2
+                    response = cheat_progress(cookies, headers, params,
+                                   classId, chapterId, lessionId,
+                                   courseId, classTaskId, reportTime,
+                                   progress, reportType)
+                    logger.info("当前课程正在观看 开始刷新")
+                    if response.status_code != 200:
+                        logger.error("刷新记录失败 报错信息:{}".format(response.text))
+                    continue
+
+        count += 1
+        logger.info("等待30s继续刷新记录")
+        time.sleep(30)
+except Exception as e:
+    logger.exception("程序错误退出")  # 捕获所有异常并记录堆栈信息
+
+time.sleep(3)  # 程序结束前等待3秒
